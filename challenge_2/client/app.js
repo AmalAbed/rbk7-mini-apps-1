1326d7d48503c397473b7726572dee5df1abd4d8
AmalAbed <amalaljarousha93@gmail.com>
2019-10-29 22:06:08 +0200

    challenge2

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,48 @@
 
-
+var data = $('#user_info')
+// {url: '/upload_json',
+//  type: 'post',
+//  data: {info:data},
+//  success: function() {
+//          console.log('success');},
+//  error : function(){ 
+//           console.log('error');}})
+// var data = $('#user_info');
+// var options = {
+//     success: function() {
+//      console.log('success');
+//     }, type: 'post', 
+//     dataType: 'json',  url:'/upload_json',
+//     data: {info:data}, error : function(){ console.log('error');}
+// }    
+//   $("#form_info").submit(function(){
+//     event.preventDefault();
+//          $(this).ajaxSubmit(options);
+//          return false;
+//   })
+// $("#form_info").submit(function(event){
+// 	event.preventDefault(); //prevent default action 
+// 	var post_url = $(this).attr("action"); //get form action url
+// 	var request_method = $(this).attr("method"); //get form GET/POST method
+// 	var form_data = $(this).serialize(); //Encode form elements for submission
+	
+// 	$.ajax({
+// 		url : post_url,
+// 		type: request_method,
+// 		data : form_data
+// 	}).done(function(response){ //
+// 		$("#server-results").html(response);
+//     });
+//     retrieveData(); 
+// });
+    
+//  var retrieveData = function(){
+//     $.ajax({
+//         url:'http://localhost:1128/cvsfile',
+//         method:"GET",
+//         success: function() {
+//             console.log('success get data');
+//         },
+//           error : function(){ console.log('error get data');}
+//          })
+//       }   
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,51 @@
+const express = require('express');
+const app = express();
+const port = 1128;
+const path = require('path');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('client'));
 
+app.post("/", function(req,res) {
+   var obj = req.body.submit;
+   splitKeysObj(obj);
+   let csvContent = "data:text/csv;charset=utf-8,";
+   
+});
+app.get("/", function(req,res) {
+  res.send('hello');
+});
+app.listen(1128, function(){
+    console.log("Listening on 1128")
+});
 
+var splitKeysObj = function(obj){
+  debugger;
+ var keyArray = Object.keys(obj);
+ var str1 = keyArray[0];
+ for(var i = 1;i < keyArray.length ; i++){
+   str1 = str1 + "," + keyArray[i];
+ }
+ str1 = str1 + "\n";
+ var str="";
+ var getData = function(values){
+   for(var key in values){
+     str = str + values[key] + ",";
+   }
+   str = str + "\n"
+   if(values.children.length === 0){
+     return str;
+   }
+   else{
+     for(var i = 0;i < values.children.length;i++){
+        str = str + "\n" + getData(values.children[i]);
+     }
+   } 
+     
+ }
+ getData(obj);
+ str1 = str1 + str;
+ return str1;
+}
+module.exports = app;
